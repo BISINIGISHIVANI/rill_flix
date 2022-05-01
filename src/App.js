870a03ed9cddacf6a43564b54bfdbba4f1528be8
BISINIGISHIVANI <bisinigishivani@gmail.com>
2022-05-01 09:48:37 +0530

    [feat]:add routes to homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
-
-
+import { Navbar,Sidebar,Footer,VideoCard } from "./componentes";
+import { HomePage, LandingPage,VideoList } from "./pages";
+import {Routes,Route} from "react-router-dom";
 function App() {
   return (
     <div className="App">
-     
+     <Navbar/>
+     <Routes>
+       <Route path="/" element={<LandingPage/>}/>
+       <Route path="/home" element={<HomePage/>}/>
+    </Routes>
+     <Footer/>
     </div>
   );
 }
