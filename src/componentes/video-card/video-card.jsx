790ed0aebc69c6f1eeb52e4b710c0e7982adf015
BISINIGISHIVANI <bisinigishivani@gmail.com>
2022-05-05 14:11:18 +0530

    [feat]:display videos

--- src/componentes/video-card/video-card.jsx
+++ src/componentes/video-card/video-card.jsx
@@ -1,49 +1,60 @@
-import {minion} from "../../assests/images/index";
 import { avatarIcn } from "../../assests/icons/icon";
 import "./video-card.css";
 import {useState} from "react";
-export default function VideoCard(){
+export default function VideoCard({
+    thumbnailImg,
+    id,
+    videoSubtitle,
+    videoTitle,
+    videoCategory,
+    videoSpan,
+    videoViews,
+    videoPublishedYear
+}){
     const [isMore,setIsMore]=useState(false);
     const BtnMoreToggle=()=>{
         setIsMore((value)=>!value)
     }
     return (
-        <div className="video-card-set">
+        <div className="video-card-set"key={id}>
             <div className="videocard-img position-relative">
-                <img className=" "src={minion}alt=""/>
+                <img src={thumbnailImg}alt="video-image"/>
                 <div className="position-absolute video-timer">
-                <span>05:00</span>
+                <span>{videoSpan}</span>
                 </div>
             </div>
-            <div className="">
+            <div>
                 <div className="flex-row flex-center flex-space-between">
-                <div>
-                    <img className="video-profile" src={avatarIcn}alt=""/>
-                </div>
-                <div className="views-date-info flex-col">
-                    <div className="flex-row flex-center gap-md">
-                        <div>
-                            <h3>minion despicable</h3>
-                            <span>unknown</span>
+                    <div>
+                        <img className="video-profile" src={avatarIcn}alt="icons"/>
+                    </div>
+                    <div className="views-date-info flex-col">
+                        <div className="flex-row flex-center gap-md">
+                            <div>
+                                <h3>{videoTitle}</h3>
+                                <span>{videoSubtitle}</span>
+                            </div>
                         </div>
-                        <div>
+                    </div>
+                    <div>
                         <div className="position-relative">
                             <i onClick={BtnMoreToggle}className="fa-solid fa-ellipsis-vertical"></i>
                         </div>
                         {isMore && 
-                        (<div className="position-absolute flex-col gap">
-                            <label> <i onClick={BtnMoreToggle}class="fa-solid fa-thumbs-up like "></i>Like</label>
-                            <label htmlFor=""><i class="fa-solid fa-bookmark watch-later"></i>WatchLater</label>
-                            <label htmlFor=""><i class="fa-solid fa-folder-plus playlist"></i> Bookmark</label>
+                        (<div className="position-absolute flex-col gap more-box">
+                            <label className="hover-white"> <i onClick={BtnMoreToggle}className="fa-solid fa-thumbs-up like "></i>Like</label>
+                            <label><i className="fa-solid fa-bookmark watch-later"></i>Bookmark</label>
+                            <label><i className="fa-solid fa-folder-plus playlist"></i> WatchLater</label>
                         </div>
                         )}
-                       </div>
-                    </div>
-                    <div>
-                    <span>2.7M Views</span>
-                    <span>.1 year ago</span>
                     </div>
                 </div>
+                <div className="flex-row flex-center flex-space-between">
+                    <div></div>
+                    <div>
+                    <span>{videoViews} Views</span>
+                    <span>.{videoPublishedYear}</span>
+                    </div>
                 </div>
             </div>
         </div>
--- src/pages/video-listing/video-listing.jsx
+++ src/pages/video-listing/video-listing.jsx
@@ -1,25 +1,43 @@
 import { VideoCard } from "../../componentes";
 import "./video-list.css";
+import { useState,useEffect } from "react";
+import { useSearchFilter} from "../../hooks";
+import {GetSearchData} from "../../hooks/utils/index"
+import { useVideos } from "../../hooks/context/video-context";
 export default function VideoList(){
+    const {videos}=useVideos();;
+    const {videoState:{searchQuery}}=useSearchFilter();
+    const searchData=GetSearchData(videos,searchQuery);
     return (
         <div className="video-list-set">
             <div className="chips-bar flex-row gap padding-md bd-bottom margin-md">
-                <div className="chip chip-active">
+                <div className={`chips chip-active } `}>
                     <p>All</p>
                 </div>
                 <div>
-                    <p>short films</p>
+                    <p>Movies</p>
                 </div>
                 <div>
-                    <p>Movie</p>
+                    <p>Short film</p>
                 </div>
-                <div>
+                <div >
                     <p>Fantacy</p>
+                </div>
+                <div >
+                    <p>Motivational</p>
                 </div>
             </div>
             <div className="video-list cursor-pointer">
-                {[1,2,3,4,5,6,7,8].map((id)=>(
-                    <VideoCard key={id}/>
+                {searchData.map(({_id,thumbNail,categoryName,title,subtitle,videoSpan,views,publishedYear})=>(
+                    <VideoCard key={_id} 
+                    thumbnailImg={thumbNail}
+                    videoTitle={title}
+                    videoCategory={categoryName}
+                    videoPublishedYear={publishedYear}
+                    videoSpan={videoSpan}
+                    videoViews={views}
+                    videoSubtitle={subtitle}
+                    />
                 ))}
             </div>
         </div>
