d8ae57275b8d390378e000398314a83bd5c88d55
BISINIGISHIVANI <bisinigishivani@gmail.com>
2022-05-05 14:07:12 +0530

    [feat]:create use videos

---
+++ src/hooks/context/video-context.jsx
@@ -0,0 +1,12 @@
+import { createContext ,useContext,useState,useEffect} from "react";
+import { getVideoData } from "../utils/video-utils/video-handler";
+
+const VideoContext=createContext(null);
+export const useVideos=()=>useContext(VideoContext);
+export const VideoProvider=({children})=>{
+    const [videos,setVideos]=useState([])
+    useEffect(()=>getVideoData(setVideos),[])
+    return <VideoContext.Provider value={{videos}}>
+        {children}
+    </VideoContext.Provider>
+}
