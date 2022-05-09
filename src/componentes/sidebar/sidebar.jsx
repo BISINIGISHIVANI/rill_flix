66205d895b1bab615ba1ac473ce29e5f33018f95
BISINIGISHIVANI <bisinigishivani@gmail.com>
2022-05-09 22:50:11 +0530

    [refactor]:add link to sidebar

--- src/componentes/sidebar/sidebar.jsx
+++ src/componentes/sidebar/sidebar.jsx
@@ -1,6 +1,7 @@
 import "./sidebar.css";
 import { ExploreIcn,historyIcn,likedIcn,moreIcn,watchlaterIcn} from "../../assests/icons/icon";
 import { useState, useCallback } from "react";
+import { Link } from "react-router-dom";
 export default function Sidebar() {
   const [sideBar, setSideBar] = useState(false);
   const sideBarToggle = useCallback(() => setSideBar((value) => !value),[]);
@@ -10,18 +11,21 @@ export default function Sidebar() {
         <ul className="flex-col gap-md flex-center cursor-pointer ">
           <div className="flex-row gap list-none">
             <div className="flex-col icon-set" onClick={sideBarToggle}>
-              <li><img  src={ExploreIcn}alt="explore-icon" /></li>
+            <Link to="/home">
+              <li><img  src={ExploreIcn}alt="explore-icon" /></li></Link>
               <li><img  src={historyIcn} alt="toggle-icn" /></li>
               <li><img  src={watchlaterIcn}alt="toggle-icn" /></li>
-              <li><img src={likedIcn}alt="toggle-icn"/></li>
+              <Link to="/likes"><li><img src={likedIcn}alt="toggle-icn"/></li></Link>
               <li><img src={moreIcn}alt="toggle-icn"/></li>
             </div>
             {sideBar && (
               <div className="flex-col icn-content">
-                <li className="margin-top"> Explore</li>
+                <Link to="/home">
+                <li className="margin-top"> Explore</li> </Link>
                 <li>History</li>
                 <li> Watchlater</li>
-                <li>Liked videos</li>
+                <Link to="/likes">
+                <li>Liked videos</li></Link>
                 <li> More <i class="fa-solid fa-circle-chevron-down"></i></li>
                 <div>
                   <li>Kids</li>
