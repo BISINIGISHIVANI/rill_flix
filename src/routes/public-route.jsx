53bb7a0dfada679424f546aa7e9fbed72cb0fcfa
BISINIGISHIVANI <bisinigishivani@gmail.com>
2022-06-29 13:18:13 +0530

    feat:add mockmanjs

--- src/routes/public-route.jsx
+++ src/routes/public-route.jsx
@@ -4,17 +4,23 @@ import { LandingPage } from "../pages";
 import { SignInPage, SignUpPage, ForgotPage } from "../pages/authentication";
 import { VideoLikedPage } from "../pages/video-liked/video-liked";
 import { VideoWatchLaterPage } from "../pages/video-watchlater/video-watchlater";
-import { VideoPlaylistPage } from "../pages/video-playlist/video-playlist";
-import { SinglePlaylistPage } from "../pages/single-playlist/single-playlist";
+import { VideoPlaylistPage } from "../pages/video-playlist/video-playlists";
+import { SinglePlaylistPage } from "../pages/single-playlist/single-playlists";
 import {SingleVideoPage} from "../pages/single-video/single-video";
 import { VideoHistoryPage } from "../pages/video-history/video-history";
 import{ VideoList} from "../pages";
 import {ToastContainer} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { usePlaylistModal } from "../hooks/context/playlistModal-context";
+import { PlaylistModal } from "../componentes/playlist-model/playlist-modal";
+import MockmanEs from "mockman-js";
 export default function PublicRoute() {
+ const {modalState:{showModal}}=usePlaylistModal()
   return (
     <div>
+      {showModal ? <PlaylistModal /> : ""}
       <Routes>
+        <Route path="/mock" element={<MockmanEs />} />
         <Route path="/" element={<LandingPage />} />
         <Route path="/explore" element={<VideoList />} />
         <Route path="/signin" element={<SignInPage />} />
@@ -23,9 +29,9 @@ export default function PublicRoute() {
         <Route path="/video/:videoId" element={<SingleVideoPage />} />
         <Route path="/likes" element={<VideoLikedPage />} />
         <Route path="/watchlater" element={<VideoWatchLaterPage />} />
-        <Route path="/history"element={<VideoHistoryPage/>}/>
+        <Route path="/history" element={<VideoHistoryPage />} />
         <Route path="/playlist" element={<VideoPlaylistPage />} />
-        <Route path="playlist/:playlistId" element={<SinglePlaylistPage />} />
+        <Route path="/playlist/:playlistId" element={<SinglePlaylistPage />} />
       </Routes>
       <Footer />
       <ToastContainer
